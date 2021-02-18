6ab658e08deeabe1c2e2b6a269e7063a802a839f
woosikyang <woosik1505@korea.ac.kr>
2021-02-18 22:37:22 +0900

    baseline code cleansing

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
+from sklearn.model_selection import KFold
 from tqdm import tqdm
-import imutils
-import zipfile
+#import imutils
+#import zipfile
 import os
 from PIL import Image
-
-
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.models as models
 import torchvision.transforms as T
 from torch.utils.data import DataLoader, Dataset
-
+from model import *
+from utils import *
 
 device = torch.device('cuda:0')
 
@@ -27,98 +23,23 @@ namelist = os.listdir('./data/dirty_mnist_2nd/')
 
 
 # unmpy를 tensor로 변환하는 ToTensor 정의
-class ToTensor(object):
-    """numpy array를 tensor(torch)로 변환합니다."""
-
-    def __call__(self, sample):
-        image, label = sample['image'], sample['label']
-        # swap color axis because
-        # numpy image: H x W x C
-        # torch image: C X H X W
-        image = image.transpose((2, 0, 1))
-        return {'image': torch.FloatTensor(image),
-                'label': torch.FloatTensor(label)}
 
 
 # to_tensor 선언
-to_tensor = T.Compose([
-    ToTensor()
-])
+to_tensor = T.Compose([ToTensor()])
 
 
-class DatasetMNIST(torch.utils.data.Dataset):
-    def __init__(self,
-                 dir_path,
-                 meta_df,
-                 transforms=to_tensor,  # 미리 선언한 to_tensor를 transforms로 받음
-                 augmentations=None):
-        self.dir_path = dir_path  # 데이터의 이미지가 저장된 디렉터리 경로
-        self.meta_df = meta_df  # 데이터의 인덱스와 정답지가 들어있는 DataFrame
-
-        self.transforms = transforms  # Transform
-        self.augmentations = augmentations  # Augmentation
-
-    def __len__(self):
-        return len(self.meta_df)
-
-    def __getitem__(self, index):
-        # 폴더 경로 + 이미지 이름 + .png => 파일의 경로
-        # 참고) "12".zfill(5) => 000012
-        #       "146".zfill(5) => 000145
-        # cv2.IMREAD_GRAYSCALE : png파일을 채널이 1개인 GRAYSCALE로 읽음
-        image = cv2.imread(self.dir_path + \
-                           str(self.meta_df.iloc[index, 0]).zfill(5) + '.png',
-                           cv2.IMREAD_GRAYSCALE)
-        # 0 ~ 255의 값을 갖고 크기가 (256,256)인 numpy array를
-        # 0 ~ 1 사이의 실수를 갖고 크기가 (256,256,1)인 numpy array로 변환
-        image = (image / 255).astype('float')[..., np.newaxis]
-
-        # 정답 numpy array생성(존재하면 1 없으면 0)
-        label = self.meta_df.iloc[index, 1:].values.astype('float')
-        sample = {'image': image, 'label': label}
-
-        # transform 적용
-        # numpy to tensor
-        if self.transforms:
-            sample = self.transforms(sample)
-
-        # sample 반환
-        return sample
 '''  
 model
 '''
-# nn.Module을 상속 받아 MultiLabelResnet를 정의
-class MultiLabelResnet(nn.Module):
-    def __init__(self):
-        super(MultiLabelResnet, self).__init__()
-        self.conv2d = nn.Conv2d(1, 3, 3, stride=1)
-        self.resnet = models.resnet18()
-        self.FC = nn.Linear(1000, 26)
 
-    def forward(self, x):
-        # resnet의 입력은 [3, N, N]으로
-        # 3개의 채널을 갖기 때문에
-        # resnet 입력 전에 conv2d를 한 층 추가
-        x = F.relu(self.conv2d(x))
-
-        # resnet18을 추가
-        x = F.relu(self.resnet(x))
-
-        # 마지막 출력에 nn.Linear를 추가
-        # multilabel을 예측해야 하기 때문에
-        # softmax가 아닌 sigmoid를 적용
-        x = torch.sigmoid(self.FC(x))
-        return x
 # 모델 선언
 model = MultiLabelResnet()
-model
-
 
 '''  
 train
 '''
 # cross validation을 적용하기 위해 KFold 생성
-from sklearn.model_selection import KFold
 
 kfold = KFold(n_splits=5, shuffle=True, random_state=0)
 
